9c54ce91dc44c94818061040b58b575a7064f5f0
Sahily <sahily.exe@gmail.com>
2022-11-23 23:20:26 -0500

    arreglo

--- src/app/componentes/navegacion/navegacion.component.ts
+++ src/app/componentes/navegacion/navegacion.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
+import { Amplify } from 'aws-amplify';
+import awsExports from '../../aws-exports';
 
 @Component({
   selector: 'app-navegacion',
@@ -8,7 +11,9 @@ import { Router } from '@angular/router';
 })
 export class NavegacionComponent implements OnInit {
   cantidadArticulos:number=0;
-  constructor(private router:Router) {}
+  constructor(private router:Router,public authenticator: AuthenticatorService) {
+    Amplify.configure(awsExports);
+  }
   
   ngOnInit(): void {
   }
