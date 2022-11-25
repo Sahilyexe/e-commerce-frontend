61782680f348f26f2ae212cde21faad90118e65c
Sahily <sahily.exe@gmail.com>
2022-11-25 00:02:00 -0500

    se remueven comentarios del servicio productos

--- src/app/servicios/productos/productos.service.ts
+++ src/app/servicios/productos/productos.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ProductoModel } from 'src/app/model/producto';
-import * as productosJson from '../../../assets/productos.json';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
-  producto = new ProductoModel();
-  productos:ProductoModel [] =[];
+
   constructor( private http:HttpClient) { }
 
   getProductos(){
     return this.http.get(`${environment.url}getProductos`)
   }
   getProducto(id:string){
-    // this.getProductos();
-    // let producto :ProductoModel=new ProductoModel();
-    //  for( let i= 0; i< this.productos.length;i++){
-    //      producto= this.productos[i];
-    //      if(producto.id == id){
-    //       return producto;
-    //     } 
-    //  }
    return this.http.get(`${environment.url}getProducto/${id}`);
   }
-crearProducto(producto:any){
-   const headers = new HttpHeaders();
-   headers.append('Content-Type','multipart/form-data')
-  return this.http.post(`${environment.url}crearproducto`,producto/*,{headers: headers}*/)
+  crearProducto(producto:any){
+  return this.http.post(`${environment.url}crearproducto`,producto)
 }
 }
